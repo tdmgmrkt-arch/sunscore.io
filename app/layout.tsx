134b8cbdc28e604d2b2bcf44e3a85e184589b352
kennethlerch <kenneth.lerch@gmail.com>
2026-01-19 15:50:28 -0800

    google analytics tag implemented into layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 import CookieConsent from "@/components/CookieConsent";
 
@@ -62,6 +63,7 @@ export default function RootLayout({
       >
         {children}
         <CookieConsent />
+        <GoogleAnalytics gaId="G-ER6S3BV018" />
       </body>
     </html>
   );
